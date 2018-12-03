2615a18649a97fc467dc22874f15dbd7e4a1385c
Benedict Aas <x@shou.io>
2018-12-03 15:52:02 +0000

    stats

--- Stats.hs
+++ Stats.hs
@@ -1,8 +1,27 @@
 
-{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeInType, MultiParamTypeClasses, AllowAmbiguousTypes, ScopedTypeVariables #-}
 
 import Data.Proxy
 import GHC.TypeLits
+
+
+type a |= b = b
+
+data Biased
+data Unbiased
+
+class Variance a where
+  variance :: forall a b. Floating b => [b] -> a |= b
+  covariance :: forall a b. Floating b => [b] -> [b] -> a |= b
+
+instance Variance Unbiased where
+  variance xs = varianceBuilder xs (-1)
+  covariance xs ys = covarianceBuilder xs ys (-1)
+
+instance Variance Biased where
+  variance xs = varianceBuilder xs 0
+  covariance xs ys = covarianceBuilder xs ys 0
+
 
 mean xs = sum xs / fromIntegral (length xs)
 
@@ -24,21 +43,35 @@ featureScaling xs = [ (x - xMin) / (xMax - xMin) | x <- xs ]
 
 expectedValue xs ps = sum $ zipWith (*) xs ps
 
-data UnbiasedSample
-class Floating b => Variance (a :: Symbol) b where
-  variance :: Proxy a -> [b] -> b
+varianceBuilder xs bias =
+  expectedValue xs' (replicate n (1 / fromIntegral (bias + n)))
+    where
+      xMean = mean xs
+      xs' = flip map xs $ \x -> (x - xMean)**2
+      n = length xs
 
-instance Variance UnbiasedSample where
-  variance = undefinend
+covarianceBuilder xs ys bias =
+  sum xys / fromIntegral (bias + n)
+    where
+      xMean = mean xs
+      yMean = mean ys
+      xs' = map (+ negate xMean) xs
+      ys' = map (+ negate yMean) ys
+      xys = zipWith (*) xs' ys'
+      n = length xys
 
--- | Unbiased sample variance
-variance' xs = expectedValue xs' (replicate n (1 / fromIntegral (n - 1)))
-  where
-    xMean = mean xs
-    xs' = flip map xs $ \x -> (x - xMean)**2
-    n = length xs
+sd :: forall a b. (Variance a, Floating b) => [b] -> b
+sd = sqrt . variance @a
 
-sd = sqrt . variance
+corr :: forall a b. (Variance a, Floating b) => [b] -> [b] -> b
+corr xs ys = covariance @a xs ys / (sqrt $ variance @a xs * variance @a ys)
 
-covariance = undefined
+correlation xs ys = xys / rootedSquareSums
+    where
+      meanX = sum xs / fromIntegral (length xs)
+      meanY = sum ys / fromIntegral (length ys)
+      xs' = map (+ negate meanX) xs
+      ys' = map (+ negate meanY) ys
+      xys = sum $ zipWith (*) xs' ys'
+      rootedSquareSums = sqrt $ sum [x**2|x<-xs'] * sum [y**2|y<-ys']
 
